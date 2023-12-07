0586f14de86554f87fb1e2ddabd50f1ae4b6be90
EdmundHernandezStudent <111832108+EdmundHernandezStudent@users.noreply.github.com>
2023-12-06 23:45:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const port = 3000
 app.set("view engine", "ejs")
 
 app.use(express.static("public"))
-app.use("/CSS",express.static(__dirname + "public/JS"))
+app.use("/CSS",express.static(__dirname))
 
 //Need to add the game files to view
 app.get('/', (req, res) => {
-res.render('')
+res.render('Final ProjectV2')
+})
+
+app.get('/scoreboard', (req, res) => {
+res.render('FPScoreboard')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
